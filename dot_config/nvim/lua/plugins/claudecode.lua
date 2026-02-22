a36d1edf584e2f982d0f4990f07b3af488ae6aee
rym49 <hatanakaryoma.130@gmail.com>
2026-02-23 06:41:32 +0900

    claudecode.nvim: openFileハンドラを無効化してファイル自動オープンを抑止

--- dot_config/nvim/lua/plugins/claudecode.lua
+++ dot_config/nvim/lua/plugins/claudecode.lua
@@ -11,6 +11,21 @@ return {
       vertical_split = true,
     },
   },
+  config = function(_, opts)
+    require("claudecode").setup(opts)
+
+    -- openFile ツールのハンドラを差し替え、ファイルを開かないようにする
+    local tools = require("claudecode.tools")
+    if tools.tools.openFile then
+      tools.tools.openFile.handler = function(params)
+        return {
+          content = {
+            { type = "text", text = "Opened file: " .. (params.filePath or "") },
+          },
+        }
+      end
+    end
+  end,
   keys = {
     { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "選択範囲を送信" },
     { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "バッファを追加" },
