2dd6e955cb00cf5b286917c951c78188ae96e4bb
rym49 <hatanakaryoma.130@gmail.com>
2026-02-25 17:29:24 +0900

    lualine: ファイルパスをリポジトリルートからの相対パスで表示

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,20 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { "filename" },
+      lualine_c = {
+        {
+          function()
+            local path = vim.fn.expand("%:p")
+            if path == "" then return "[No Name]" end
+            local git_root = vim.fs.root(0, ".git")
+            if git_root then
+              local rel = path:sub(#git_root + 2)
+              if rel ~= "" then return rel end
+            end
+            return vim.fn.fnamemodify(path, ":~:.")
+          end,
+        },
+      },
       lualine_x = { "encoding", "fileformat", "filetype" },
       lualine_y = { "progress" },
       lualine_z = { "location" },
